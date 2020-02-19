ae78146dbc1c6bb350e0fdd73d707681a08f9316
PROPHESSOR <xenteze@gmail.com>
2020-02-19 17:20:31 +0200

    Попытка работать с midi-writer-js
    Увы, @types для него пока нет :/

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,26 @@ import Note from "./Note";
 import { randomChoice } from "./Utils";
 import { intervals } from "./Interval";
 
+import fs from 'fs';
+
+import Midi from 'midi-writer-js';
+
 function main() {
+  const track = new Midi.Track();
+
   const notes = generateFewNotes();
 
+  const note = new Midi.NoteEvent({
+    pitch: ["C4", "D4", "E4"],
+    duration: "4"
+  });
+
+  track.addEvent(notes.map(note => new Midi.NoteEvent({pitch: note.toLetter(), duration: "4"})));
+
   console.log(notes.map(note => note.toLetter()).join(' '));
+
+  const writer = new Midi.Writer(track);
+  fs.writeFileSync('test.mid', writer.buildFile());
 }
 
 /**
